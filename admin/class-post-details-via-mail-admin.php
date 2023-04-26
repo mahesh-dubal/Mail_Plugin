d9a1ec3d4f767876a785d2e288212605edb3cdde
mahesh4320 <122158938+mahesh4320@users.noreply.github.com>
2023-04-26 15:02:46 +0530

    get_page_speed_score() updated for API limit is over

--- admin/class-post-details-via-mail-admin.php
+++ admin/class-post-details-via-mail-admin.php
@@ -172,24 +172,28 @@ class Post_Details_Via_Mail_Admin
 	//To get google page speed score
 	public function get_page_speed_score($url)
 	{
+		$api_key = "416ca0ef-63e4-4caa-a047-ead672ecc874"; // your api key
+		$new_url = "http://www.webpagetest.org/runtest.php?url=" . $url . "&runs=1&f=xml&k=" . $api_key;
+		$run_result = simplexml_load_file($new_url);
+		$status = $run_result->statusCode;
+		if ($status == 400) {
+			$error = "API Limit is Over!";
+			return $error;
+		} else {
+			$test_id = $run_result->data->testId;
 
-		$api_key = "416ca0ef-63e4-4caa-a047-ead672ecc874"; // Google Cloud Platform API key
-		$result_url = "http://www.webpagetest.org/runtest.php?url=" . $url . "&runs=1&f=xml&k=" . $api_key;
-		$run_result = simplexml_load_file($result_url);
-		$test_id = $run_result->data->testId;
+			$status_code = 100;
 
-		$status_code = 100;
+			while ($status_code != 200) {
+				sleep(10);
+				$xml_result = "http://www.webpagetest.org/xmlResult/" . $test_id . "/";
+				$result = simplexml_load_file($xml_result);
+				$status_code = $result->statusCode;
+				$time = (float) ($result->data->median->firstView->loadTime) / 1000;
+			};
 
-		while ($status_code != 200) {
-			sleep(10);
-			$xml_result = "http://www.webpagetest.org/xmlResult/" . $test_id . "/";
-			$result = simplexml_load_file($xml_result);
-			$status_code = $result->statusCode;
-			$page_speed_score = (float) ($result->data->median->firstView->loadTime) / 1000;
+			return $time;
 		}
-
-
-		return $page_speed_score;
 	}
 
 
